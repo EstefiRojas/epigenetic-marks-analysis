82a3fdf95b7d0f6b64d58b3b6b021b40896318b9
Karla Rojas <krojas@postgrad.otago.ac.nz>
2024-03-21 09:27:28 +1300

    Scripts used to analyze chromatin accessibility and methylome

--- scripts/run_chrm_acc.sh
+++ scripts/run_chrm_acc.sh
@@ -11,11 +11,11 @@ fi
 
 # LONG NCRNA
 ## Process histone marks for exon1
-sh chrm_acc_processing.sh ../data/functional-lncrna-exon1-dataset.csv chrm_acc lncrna-exon1-histone-feature
-sh chrm_acc_processing.sh ../data/lncrna-exon1-negative-control-dataset.csv chrm_acc lncrna-exon1-NC-histone-feature
+sh chrm_acc_processing.sh ../data/functional-lncrna-exon1-dataset.csv lncrna-exon1-histone-feature
+sh chrm_acc_processing.sh ../data/lncrna-exon1-negative-control-dataset.csv lncrna-exon1-NC-histone-feature
 ## Process histone marks for exon2
-sh chrm_acc_processing.sh ../data/functional-lncrna-exon2-dataset.csv chrm_acc lncrna-exon2-histone-feature
-sh chrm_acc_processing.sh ../data/lncrna-exon2-negative-control-dataset.csv chrm_acc lncrna-exon2-NC-histone-feature
+sh chrm_acc_processing.sh ../data/functional-lncrna-exon2-dataset.csv lncrna-exon2-histone-feature
+sh chrm_acc_processing.sh ../data/lncrna-exon2-negative-control-dataset.csv lncrna-exon2-NC-histone-feature
 ## Join datasets into one for R script
 sh join_datasets.sh ../data/chrm_acc_feature/chrm_acc/chrm_acc_lncrna-exon1-histone-feature.csv ../data/chrm_acc_feature/chrm_acc/chrm_acc_lncrna-exon2-histone-feature.csv ../data/chrm_acc_feature/chrm_acc/chrm_acc_lncrna_positives_matrix
 sh join_datasets.sh ../data/chrm_acc_feature/chrm_acc/chrm_acc_lncrna-exon1-NC-histone-feature.csv ../data/chrm_acc_feature/chrm_acc/chrm_acc_lncrna-exon2-NC-histone-feature.csv ../data/chrm_acc_feature/chrm_acc/chrm_acc_lncrna_negatives_matrix
@@ -24,11 +24,11 @@ sh join_datasets.sh ../data/chrm_acc_feature/chrm_acc/chrm_acc_lncrna_positives_
 #SHORT NCRNA
 sh chrm_acc_processing.sh \
     ../data/functional-short-ncrna-dataset.csv \
-    chrm_acc \
+    \
     short-ncrna-histone-feature
 sh chrm_acc_processing.sh \
     ../data/short-ncrna-negative-control-dataset.csv \
-    chrm_acc \
+    \
     short-ncrna-NC-histone-feature
 sh join_datasets.sh \
     ../data/chrm_acc_feature/chrm_acc/chrm_acc_short-ncrna-histone-feature.csv \
@@ -37,11 +37,11 @@ sh join_datasets.sh \
 
 
 # PROTEIN CODING
-sh chrm_acc_processing.sh ../data/protein-exon2-dataset.csv chrm_acc protein-exon2-histone-feature
-sh chrm_acc_processing.sh ../data/protein-exon2-negative-control-dataset.csv chrm_acc protein-exon2-NC-histone-feature
+sh chrm_acc_processing.sh ../data/protein-exon2-dataset.csv protein-exon2-histone-feature
+sh chrm_acc_processing.sh ../data/protein-exon2-negative-control-dataset.csv protein-exon2-NC-histone-feature
 
-sh chrm_acc_processing.sh ../data/protein-exon3-dataset.csv chrm_acc protein-exon3-histone-feature
-sh chrm_acc_processing.sh ../data/protein-exon3-negative-control-dataset.csv chrm_acc protein-exon3-NC-histone-feature
+sh chrm_acc_processing.sh ../data/protein-exon3-dataset.csv protein-exon3-histone-feature
+sh chrm_acc_processing.sh ../data/protein-exon3-negative-control-dataset.csv protein-exon3-NC-histone-feature
 
 sh join_datasets.sh ../data/chrm_acc_feature/chrm_acc/chrm_acc_protein-exon2-histone-feature.csv ../data/chrm_acc_feature/chrm_acc/chrm_acc_protein-exon3-histone-feature.csv ../data/chrm_acc_feature/chrm_acc/chrm_acc_protein_positives_matrix
 sh join_datasets.sh ../data/chrm_acc_feature/chrm_acc/chrm_acc_protein-exon2-NC-histone-feature.csv ../data/chrm_acc_feature/chrm_acc/chrm_acc_protein-exon3-NC-histone-feature.csv ../data/chrm_acc_feature/chrm_acc/chrm_acc_protein_negatives_matrix
